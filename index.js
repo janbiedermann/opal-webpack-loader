85665458142b09df5dc72ade5a249c95bb1c37fd
Jan Biedermann <jan@kursator.de>
2019-09-10 17:30:26 +0200

    Use typeof.

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ function start_compile_server() {
 function initialize_options(that) {
     const options = loaderUtils.getOptions(that);
     Object.keys(default_options).forEach(
-        (key) => { if (options[key] === undefined) options[key] = default_options[key]; }
+        (key) => { if (typeof options[key] === 'undefined') options[key] = default_options[key]; }
     );
     return options;
 }
